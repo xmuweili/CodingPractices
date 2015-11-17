befb71ea5f9bce4629ff39fc1a15ead9b40d266a
uhliwei <xmuweili@gmail.com>
2015-11-16 23:48:25 -0600

    corrent version.

--- Graph_And_Tree/Unique Binary Search Trees II.cpp
+++ Graph_And_Tree/Unique Binary Search Trees II.cpp
@@ -9,44 +9,29 @@
  */
 class Solution {
 public:
-	TreeNode* duplicate(TreeNode* root) {
-		if(root == NULL) return NULL;
-		TreeNode* d_root = new TreeNode(root->val);
-		d_root->left = duplicate(root->left);
-		d_root->right = duplicate(root->right);
-		return root;
-	}
 
-	vector<TreeNode*> generateLeft(int start, int end, vector<TreeNode*> root) {
-		if(start > end)
-			return root;
-		
-	}
-
-
-	vector<TreeNode*> generateLeft(int start, int end, vector<TreeNode*> root) {
-
+	vector<TreeNode*> generate(int start, int end) {
+		vector<TreeNode*> res;	
+		if(start > end) res.push_back(NULL);
+		if(start == end) res.push_back(new TreeNode(start));
+		if(start < end){
+			for (int i = start; i <= end; ++i)
+			{
+				vector<TreeNode*> left_trees = generate(start, i-1);
+				vector<TreeNode*> right_trees = generate(i+1, end);
+				for(int j=0; j<left_trees.size(); ++j)
+					for(int k=0; k<right_trees.size(); ++k) {
+						TreeNode* root = new TreeNode(i);
+						root->left = left_trees[j];
+						root->right = right_trees[k];
+						res.push_back(root);
+					}
+			}
+		}
+		return res;
 	}
 
     vector<TreeNode*> generateTrees(int n) {
-        vector<TreeNode*> res;
-        if(n == 0) return res;
-
-        if(n == 1) {
-        	TreeNode* root = new TreeNode(n);
-        	res.push_back(root);
-        	return res;
-        }
-
-        for (int i = 1; i <= count; ++i)
-        {
-        	TreeNode* root = new TreeNode(n);
-        	std::vector<TreeNode*> temp;
-        	temp.push_back(root);
-        	vector<TreeNode*> left_tree = generateLeft(1,i-1, temp);
-        	vector<TreeNode*> full_tree = generateLeft(i+1,n, left_tree);
-        	res.insert(res.begin(), full_tree.begin(), full_tree.end());
-        }
-        return res;
+    	return generate(1,n);
     }
 };
